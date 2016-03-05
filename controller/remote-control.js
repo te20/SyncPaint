37fc41d89ad7f6fb636e6d1cf0ce14d4c1adb5e6
Tetsuo Yutani <tetsuo.yutani@gmail.com>
2016-03-05 20:48:17 +0900

    Do not go down even if invalid JSON string is sent as message

--- controller/remote-control.js
+++ controller/remote-control.js
@@ -4,6 +4,23 @@ const log4js = require('log4js');
 const logger = log4js.getLogger('remote');
 logger.setLevel('TRACE');
 
+const tryParseJSON = jsonString => {
+    try {
+        const json = JSON.parse(jsonString);
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns 'null', and typeof null === "object",
+        // so we must check for that, too.
+        if (json && typeof json === 'object' && json !== null) {
+            return json;
+        }
+    } catch (e) {
+        // go through to 'return false'
+    }
+
+    return false;
+};
+
 const broadcast = (sockets, message) => {
     sockets.forEach(socket => {
         logger.info('send message: ' + message + ' to ' + socket.upgradeReq.client.remoteAddress);
@@ -18,8 +35,18 @@ module.exports = function remoteControl(server) {
 
     const WebSocketServer = require('ws').Server;
     const wss = new WebSocketServer({
-        "server": server
+        "server": server,
+        handleProtocols: (protocols, callback) => {
+            console.log('protocols: ', protocols);
+            callback(true, protocols[protocols.length - 1]);
+        }
     });
+
+    wss.broadcat = data => {
+        wss.clients.forEach(client => {
+            client.send(data);
+        });
+    };
 
     wss.on('connection', function (ws) {
         const address = ws.upgradeReq.client.remoteAddress;
@@ -32,15 +59,21 @@ module.exports = function remoteControl(server) {
         ws.on('message', function (message) {
             logger.info('received message: ' + message + ' from ' + address);
 
-            const msg = JSON.parse(message);
+            const msg = tryParseJSON(message);
+            if (!msg || !msg.hasOwnProperty('type')) {
+                return;
+            }
             switch (msg.type) {
-                case 'success_connection':
-                    ws.send(JSON.stringify({type: 'success_connection', value: 'none'}));
-                    break;
-                case 'iamalive':
-                    break;
-                default:
-                    broadcast(getOthers(connections, ws), message);
+            case 'success_connection':
+                ws.send(JSON.stringify({
+                    type: 'success_connection',
+                    value: 'none'
+                }));
+                break;
+            case 'iamalive':
+                break;
+            default:
+                broadcast(getOthers(connections, ws), message);
             }
         });
 
@@ -57,5 +90,13 @@ module.exports = function remoteControl(server) {
         ws.on('error', function (error) {
             logger.error('WebSocket Error: ' + error);
         });
+    });
+
+    wss.on('error', error => {
+        logger.debug('WS Server Error:' + error);
+    });
+
+    wss.on('headers', headers => {
+        logger.info('headers: ' + JSON.stringify(headers));
     });
 };
